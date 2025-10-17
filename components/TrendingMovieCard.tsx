889c1ebfadc3e40f9ecd742f544f79b36fda7a3b
Zoran-Janjic <janjiczoran23@gmail.com>
2025-10-17 22:41:11 +0200

    completed trening movies

--- components/TrendingMovieCard.tsx
+++ components/TrendingMovieCard.tsx
@@ -1,37 +1,82 @@
-import { images } from '@/constants/images';
-import MaskedView from '@react-native-masked-view/masked-view';
+import { images } from "@/constants/images";
+import MaskedView from "@react-native-masked-view/masked-view";
 import { Link } from "expo-router";
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
-const TrendingMovieCard = ({ movie: { movie_id, title, posterUrl }, index }: TrendingCardProps) => {
 
+interface TrendingCardProps {
+    movie: {
+        movie_id: number;
+        title: string;
+        posterUrl: string;
+    };
+    index: number;
+}
+
+const TrendingCard = ({
+    movie: { movie_id, title, posterUrl },
+    index,
+}: TrendingCardProps) => {
     return (
         <Link href={`/movie/${movie_id}`} asChild>
-            <TouchableOpacity className="w-32 relative pl-5">
-                <Image source={{ uri: posterUrl }} className="w-32 h-48 rounded-lg" resizeMode="cover" />
-                <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
-                    <MaskedView
-                        maskElement={
-                            <Text className="font-bold text-white text-6xl">{index + 1}</Text>
-                        }
-                    >
-                        <Image
-                            source={images.rankingGradient}
-                            className="size-14"
-                            resizeMode="cover"
-                        />
-                    </MaskedView>
+            <TouchableOpacity className="w-32">
+                {/* Poster image container with index overlay */}
+                <View className="relative">
+                    <Image
+                        source={{ uri: posterUrl }}
+                        className="w-32 h-48 rounded-lg"
+                        resizeMode="cover"
+                    />
 
-                    <Text
-                        className="text-sm font-bold mt-2 text-light-200"
-                        numberOfLines={2}
+                    {/* Index number in bottom-left corner of image */}
+                    <View
+                        style={{
+                            position: "absolute",
+                            left: -8,
+                            bottom: -8,
+                            zIndex: 9999,
+                            elevation: 30,
+                        }}
                     >
-                        {title}
-                    </Text>
+                        <MaskedView
+                            style={{
+                                width: 64,
+                                height: 64,
+                                overflow: "visible",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                borderRadius: 16,
+                            }}
+                            maskElement={
+                                <View className="absolute bottom-1  px-2 py-1 rounded-full">
+
+                                    <Text
+                                        className="text-6xl font-bold text-white"
+                                    >
+                                        {index + 1}
+                                    </Text>
+                                </View>
+                            }
+                        >
+                            <Image
+                                source={images.rankingGradient}
+                                style={{ width: 64, height: 64, borderRadius: 16 }}
+                                resizeMode="cover"
+                            />
+                        </MaskedView>
+                    </View>
                 </View>
+
+                {/* Title below the image */}
+                <Text
+                    className="text-sm font-bold mt-2 text-white"
+                    numberOfLines={2}
+                >
+                    {title}
+                </Text>
             </TouchableOpacity>
         </Link>
     );
 };
 
-export default TrendingMovieCard;
+export default TrendingCard;
